f7df99db22fac30c01e184b52460c841908574e3
Mateo Lopez <matelopez2830@gmail.com>
2025-04-13 15:21:30 -0300

    Mejorando mi hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ export const Hero = () => {
         <h2 className="hero-title">HOLA, MI NOMBRE ES MATEO LOPEZ</h2>
         <h2 className='hero-subtitle'>Soy Desarrollador FullStack.</h2>
         <p className="hero-p">
-          En formación con experiencia en implementación de software y conocimientos sólidos en desarrollo backend y frontend. Estoy finalizando la certificación Professional Developer en Digital House, donde me formé en tecnologías como Java, .NET, React.js, HTML, CSS, JavaScript, MySQL y metodologías ágiles. Me interesa crecer en el área del desarrollo, aplicando buenas prácticas y aprendiendo constantemente.
+          En formación con experiencia en implementación de software y una base sólida en desarrollo backend y frontend. Actualmente finalizando la certificación Professional Developer en Digital House, donde trabajé con tecnologías como Java, .NET, React.js, HTML, CSS, JavaScript, MySQL y metodologías ágiles. También tengo conocimientos en administración de servidores y uso de sistemas operativos basados en Linux. Apasionado por el desarrollo de soluciones funcionales, busco seguir creciendo profesionalmente aplicando buenas prácticas y aprendiendo constantemente.
         </p>
         <a href="#proyectos" className='hero-button'>Ver mis proyectos</a>
       </div>
